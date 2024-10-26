12796522e1842bad4a41fd2667a8e43a77127836
Mohammad Abdul Gafoor <abdul.21bce9271@vitapstudent.ac.in>
2024-10-26 18:59:06 +0530

    Create loop_control.py

---
+++ Week 1/Day 3/loop_control.py
@@ -0,0 +1,13 @@
+# Example with break
+print("Example with break:")
+for i in range(1, 11):
+    if i == 5:
+        break  # Exit the loop when i is 5
+    print(i)
+
+# Example with continue
+print("\nExample with continue:")
+for i in range(1, 11):
+    if i == 5:
+        continue  # Skip the iteration when i is 5
+    print(i)
