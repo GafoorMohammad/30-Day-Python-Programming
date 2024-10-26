350d7f37e7596a451d5713e895c998b452d932f8
Mohammad Abdul Gafoor <abdul.21bce9271@vitapstudent.ac.in>
2024-10-26 19:07:58 +0530

    Create augmented_assignment.py

---
+++ Week 1/Day 2/augmented_assignment.py
@@ -0,0 +1,21 @@
+num = 10
+print("Initial num:", num)
+
+# Using augmented assignment operators
+num += 5  # Equivalent to num = num + 5
+print("After += 5:", num)
+
+num -= 3  # Equivalent to num = num - 3
+print("After -= 3:", num)
+
+num *= 2  # Equivalent to num = num * 2
+print("After *= 2:", num)
+
+num /= 4  # Equivalent to num = num / 4
+print("After /= 4:", num)
+
+num **= 2  # Equivalent to num = num ** 2
+print("After **= 2:", num)
+
+num %= 3  # Equivalent to num = num % 3
+print("After %= 3:", num)
