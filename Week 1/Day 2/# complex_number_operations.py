036f02078f3958acf8beb124b458d89c6461f259
Mohammad Abdul Gafoor <abdul.21bce9271@vitapstudent.ac.in>
2024-10-26 19:04:32 +0530

    Create # complex_number_operations.py

---
+++ Week 1/Day 2/# complex_number_operations.py
@@ -0,0 +1,17 @@
+a = complex(2, 3)  # Create a complex number 2 + 3j
+b = complex(1, -4)  # Create a complex number 1 - 4j
+
+print(f"a = {a}")
+print(f"b = {b}")
+
+# Addition
+print(f"a + b = {a + b}")
+
+# Subtraction
+print(f"a - b = {a - b}")
+
+# Multiplication
+print(f"a * b = {a * b}")
+
+# Division
+print(f"a / b = {a / b}")
