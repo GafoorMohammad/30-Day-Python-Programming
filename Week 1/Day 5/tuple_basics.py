3478f2bba2f392322790275bdeac720b72ced273
Mohammad Abdul Gafoor <abdul.21bce9271@vitapstudent.ac.in>
2024-10-30 12:26:48 +0530

    Create tuple_basics.py

---
+++ Week 1/Day 5/tuple_basics.py
@@ -0,0 +1,8 @@
+# Creating a tuple
+coordinates = (10, 20, 30)
+
+# Accessing elements
+print(coordinates[0])  # Output: 10
+
+# Tuples are immutable, so the following would raise an error
+# coordinates[0] = 15  # TypeError: 'tuple' object does not support item assignment
