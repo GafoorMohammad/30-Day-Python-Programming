550de7caf00fc6350e44f8b72094f8d42e34ca0e
Mohammad Abdul Gafoor <abdul.21bce9271@vitapstudent.ac.in>
2024-10-30 12:23:43 +0530

    Create list_slicing.py

---
+++ Week 1/Day 5/list_slicing.py
@@ -0,0 +1,10 @@
+colors = ["red", "green", "blue", "yellow", "purple"]
+
+# Access elements from index 1 to 3
+print(colors[1:4])  # Output: ['green', 'blue', 'yellow']
+
+# Access elements from the beginning to index 2
+print(colors[:3])   # Output: ['red', 'green', 'blue']
+
+# Access elements from index 2 to the end
+print(colors[2:])   # Output: ['blue', 'yellow', 'purple']
