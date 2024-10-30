98add96263011c146955ff5deead66483f621cc5
Mohammad Abdul Gafoor <abdul.21bce9271@vitapstudent.ac.in>
2024-10-30 12:21:31 +0530

    Create basic_list_operations.py

---
+++ Week 1/Day 5/basic_list_operations.py
@@ -0,0 +1,9 @@
+# Creating a list
+fruits = ["apple", "banana", "cherry"]
+
+# Accessing elements
+print(fruits[0])  # Output: apple
+
+# Modifying elements
+fruits[1] = "blueberry"
+print(fruits)  # Output: ['apple', 'blueberry', 'cherry']
