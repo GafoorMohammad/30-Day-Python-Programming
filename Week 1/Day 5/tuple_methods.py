8fa49c539b59260493268f97ca1b787757d9c08f
Mohammad Abdul Gafoor <abdul.21bce9271@vitapstudent.ac.in>
2024-10-30 12:31:50 +0530

    Create tuple_methods.py

---
+++ Week 1/Day 5/tuple_methods.py
@@ -0,0 +1,8 @@
+# Tuple with repeated elements
+my_tuple = (1, 2, 3, 2, 2, 4)
+
+# Count occurrences of a value
+print(my_tuple.count(2))  # Output: 3
+
+# Find index of the first occurrence
+print(my_tuple.index(3))  # Output: 2
