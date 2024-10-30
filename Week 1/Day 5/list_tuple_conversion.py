eeafe7d8023c3c470d50237ef63fab0cf4b9d575
Mohammad Abdul Gafoor <abdul.21bce9271@vitapstudent.ac.in>
2024-10-30 12:32:16 +0530

    Create  list_tuple_conversion.py

---
+++ Week 1/Day 5/list_tuple_conversion.py
@@ -0,0 +1,9 @@
+# List to tuple
+my_list = [1, 2, 3]
+my_tuple = tuple(my_list)
+print(my_tuple)  # Output: (1, 2, 3)
+
+# Tuple to list
+my_new_list = list(my_tuple)
+my_new_list.append(4)
+print(my_new_list)  # Output: [1, 2, 3, 4]
