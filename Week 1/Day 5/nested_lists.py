e77efdc2fc5d2d37b2ecdbf32b4fcaa00ce95757
Mohammad Abdul Gafoor <abdul.21bce9271@vitapstudent.ac.in>
2024-10-30 12:25:11 +0530

    Create  nested_lists.py

---
+++ Week 1/Day 5/nested_lists.py
@@ -0,0 +1,8 @@
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+# Access element in the first row, second column
+print(matrix[0][1])  # Output: 2
+
+# Access each row in a loop
+for row in matrix:
+    print(row)
