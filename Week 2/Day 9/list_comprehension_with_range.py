22b6852077d3899f04ca791f206b293ce79b9127
Mohammad Abdul Gafoor <abdul.21bce9271@vitapstudent.ac.in>
2024-11-06 13:44:49 +0530

    Create list_comprehension_with_range.py

---
+++ Week 2/Day 9/list_comprehension_with_range.py
@@ -0,0 +1,3 @@
+# Using range with list comprehension to generate even numbers
+even_numbers = [x for x in range(0, 20) if x % 2 == 0]
+print(even_numbers)  # Output: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
