fef7e4d1e6a96b7ffd3c7ad1a8bb384f10fb7da4
Mohammad Abdul Gafoor <abdul.21bce9271@vitapstudent.ac.in>
2024-11-06 13:00:53 +0530

    Create sets_basics.py

---
+++ Week 1/Day 6/sets_basics.py
@@ -0,0 +1,10 @@
+# Creating a set
+fruits = {"apple", "banana", "cherry"}
+
+# Adding elements to a set
+fruits.add("orange")
+print(fruits)  # Output: {'apple', 'banana', 'cherry', 'orange'}
+
+# Removing elements from a set
+fruits.remove("banana")
+print(fruits)  # Output: {'apple', 'cherry', 'orange'}
