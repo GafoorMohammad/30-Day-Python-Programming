156f231d08f4d2a22984f7927b7f3fec4490c5b7
Mohammad Abdul Gafoor <abdul.21bce9271@vitapstudent.ac.in>
2024-11-06 13:02:15 +0530

    Create set_vs_list.py

---
+++ Week 1/Day 6/set_vs_list.py
@@ -0,0 +1,6 @@
+# List allows duplicates, set does not
+my_list = [1, 2, 2, 3, 4]
+my_set = {1, 2, 2, 3, 4}
+
+print("List:", my_list)  # Output: List: [1, 2, 2, 3, 4]
+print("Set:", my_set)    # Output: Set: {1, 2, 3, 4}
