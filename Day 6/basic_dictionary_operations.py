43ff9e78fac9a088d5947022b359b452879bc3ae
Mohammad Abdul Gafoor <abdul.21bce9271@vitapstudent.ac.in>
2024-11-06 12:56:41 +0530

    Delete Day 6 directory

--- Day 6/basic_dictionary_operations.py
+++
@@ -1,9 +0,0 @@
-# Creating a dictionary
-person = {"name": "Alice", "age": 25, "city": "New York"}
-
-# Accessing values by key
-print(person["name"])  # Output: Alice
-
-# Adding a new key-value pair
-person["profession"] = "Engineer"
-print(person)  # Output: {'name': 'Alice', 'age': 25, 'city': 'New York', 'profession': 'Engineer'}
