7c3fe91db7cdeae34fd23432493c557c642ff626
Mohammad Abdul Gafoor <abdul.21bce9271@vitapstudent.ac.in>
2024-11-06 13:01:15 +0530

    Create set_operations.py

---
+++ Week 1/Day 6/set_operations.py
@@ -0,0 +1,12 @@
+# Creating sets
+set_a = {1, 2, 3, 4}
+set_b = {3, 4, 5, 6}
+
+# Union of sets
+print(set_a | set_b)  # Output: {1, 2, 3, 4, 5, 6}
+
+# Intersection of sets
+print(set_a & set_b)  # Output: {3, 4}
+
+# Difference of sets
+print(set_a - set_b)  # Output: {1, 2}
