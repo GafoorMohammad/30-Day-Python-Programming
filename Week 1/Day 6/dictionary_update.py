ae62c09e9d44373444964c8b6267becb19547880
Mohammad Abdul Gafoor <abdul.21bce9271@vitapstudent.ac.in>
2024-11-06 12:58:11 +0530

    Create dictionary_update.py

---
+++ Week 1/Day 6/dictionary_update.py
@@ -0,0 +1,6 @@
+# Creating a dictionary
+person = {"name": "Alice", "age": 25, "city": "New York"}
+
+# Updating values in the dictionary
+person["age"] = 26
+print(person)  # Output: {'name': 'Alice', 'age': 26, 'city': 'New York'}
