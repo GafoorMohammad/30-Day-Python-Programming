c242a6febf98faeef5a134ce9a12161f2f8feef2
Mohammad Abdul Gafoor <abdul.21bce9271@vitapstudent.ac.in>
2024-11-06 12:59:10 +0530

    Create nested_dictionaries.py

---
+++ Week 1/Day 6/nested_dictionaries.py
@@ -0,0 +1,8 @@
+# Nested dictionaries
+employees = {
+    "Alice": {"position": "Manager", "salary": 70000},
+    "Bob": {"position": "Developer", "salary": 50000},
+}
+
+# Accessing nested dictionary values
+print(employees["Alice"]["position"])  # Output: Manager
